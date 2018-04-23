ac9172c2dc011adce506763f937a5c944942cf70
Hoxwo <arcturusmuse@gmail.com>
2018-04-23 19:40:42 -0400

    working on UI stuff

--- main.go
+++ main.go
@@ -1,19 +1,90 @@
 package main
 
-import "fmt"
-import "time"
-import "btc-trader-sim/coin"
-import "btc-trader-sim/trader"
-import "btc-trader-sim/exchange"
+//import "fmt"
+//import "time"
+import termui "github.com/gizak/termui"
+//import "btc-trader-sim/coin"
+//import "btc-trader-sim/trader"
+//import "btc-trader-sim/exchange"
 
 func main() {
-    currentTime := time.Date(2010, time.January, 1, 0, 0, 0, 0, time.UTC)
-    c := coin.New("bitcoin", 1.01, "FINANCE", "BULLISH")
-    t := trader.New("kc", 0.00, 0.00, nil)
-    e := exchange.New("gdax", 100.00)
+	// set the time to Jan 1st, 2010
+	//currentTime := time.Date(2010, time.January, 1, 0, 0, 0, 0, time.UTC)
+	//dayCounter := 0
+	// array of coins
+	// coinPrices = make(map[string]float32)
+	// array of maps for storing coin price history	
+	// coinPriceHistory := make([]map[string]float32, 5900)	
+
+	// set up Bitcoin, player Trader, and an exchange
+	//c := coin.New("bitcoin", 1.01, "FINANCE", "BULLISH")
+  //  	t := trader.New("kc", 0.00)
+      //  e := exchange.New("gdax", 100.00)
     
-    fmt.Printf("%s has price %.6f \n", c.Name(), c.Price())
-    fmt.Printf("%s has savings balance %.6f \n", t.Name(), t.SavingsBalance())
-    fmt.Printf("%s has volume %.6f \n", e.Name(), e.Volume())
-    fmt.Println("Date : ", currentTime.Format("01-02-2006"))
+	//coinPrice := fmt.Sprintf("%s has price %.6f \n", c.Name(), c.Price())
+    	//playerSavings := fmt.Sprintf("%s has savings balance %.6f \n", t.Name(), t.SavingsBalance())
+        //exchangeVolume := fmt.Sprintf("%s has volume %.6f \n", e.Name(), e.Volume())
+
+	err := termui.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termui.Close()
+
+	data := []int{4, 2, 1, 6, 3, 9, 1, 4, 2, 15, 14, 9, 8, 6, 10, 13, 15, 12, 10, 5, 3, 6, 1, 7, 10, 10, 14, 13, 6}
+	spl0 := termui.NewSparkline()
+	spl0.Data = data[3:]
+	spl0.Title = "Sparkline 0"
+	spl0.LineColor = termui.ColorGreen
+
+	spl1 := termui.NewSparkline()
+	spl1.Data = data
+	spl1.Title = "Sparkline 1"
+	spl1.LineColor = termui.ColorRed
+
+	spl2 := termui.NewSparkline()
+	spl2.Data = data[5:]
+	spl2.Title = "Sparkline 2"
+	spl2.LineColor = termui.ColorMagenta
+
+	// group
+	spls1 := termui.NewSparklines(spl0, spl1, spl2)
+	spls1.Height = 8
+	spls1.Width = 20
+	spls1.Y = 3
+	spls1.BorderLabel = "Group Sparklines"
+
+	// single
+        singledata := []int{13, 15, 12, 10, 5, 3, 6, 1, 7, 10, 10, 14, 13, 6, 4, 2, 1, 6, 3, 9, 1, 4, 2, 15, 14, 9, 8, 6, 10}
+	single0 := termui.NewSparkline()
+	single0.Data = singledata
+	single0.Title = "Sparkline 0"
+	single0.LineColor = termui.ColorCyan
+		
+	singlespl0 := termui.NewSparklines(single0)
+	singlespl0.Height = 2
+	singlespl0.Width = 20
+	singlespl0.Border = false
+
+	termui.Render(spls1, singlespl0)
+
+	termui.Handle("/sys/kbd/q", func(termui.Event) {
+		termui.StopLoop()
+	})
+
+	termui.Handle("/sys/kbd/i", func(termui.Event) {
+		//advance one day
+	})
+
+	termui.Loop()
+
+}
+
+func AdvanceOneDay(daysSinceStart int, coinPriceHistory []map[string]float32) {
+	//save coin price history for all coins
+
+	//find new price for all coins
+
+	//find new exchange volume for all exchanges
+
 }
--- trader/trader.go
+++ trader/trader.go
@@ -1,14 +1,18 @@
 package trader
 
+import "fmt"
+
 type trader struct {
     name string
     savingsBalance float32
-    fiatBalance float32
+    savingsBalanceHistory []float32
     coinBalancesMap map[string]float32
+    coinBalancesHistoryMap []map[string]float32
 }
 
-func New(name string, savingsBalance float32, fiatBalance float32, coinBalancesMap map[string]float32) trader {
-    t := trader {name, savingsBalance, fiatBalance, make(map[string]float32)}
+func New(name string, savingsBalance float32) trader {
+    t := trader {name, savingsBalance, make([]float32, 5900), 
+		 make(map[string]float32), make([]map[string]float32, 5900)}
     return t
 }
 
@@ -19,3 +23,49 @@ func (t trader) Name() string {
 func (t trader) SavingsBalance() float32 {
     return t.savingsBalance
 }
+
+func (t trader) SavingsBalanceOnDay(daysSinceStart int) float32 {
+    return t.savingsBalanceHistory[daysSinceStart]
+}
+
+func (t trader) RecordBalances(daysSinceStart int) {
+    // record savings balance
+    t.savingsBalanceHistory[daysSinceStart] = t.savingsBalance
+    
+    // record coin balances
+    t.coinBalancesHistoryMap[daysSinceStart] = t.coinBalancesMap
+}
+
+// coin: coin name
+// coinAmount: amount to buy or sell
+// dollarAmount: cost in fiat
+// op: 1 - BUY, 2 - SELL
+func (t trader) ModifyCoinAndSavingsBalance(coin string, coinAmount float32, dollarAmount float32, op int ) string {
+  if(op == 1) {
+	current := t.coinBalancesMap[coin]
+	currentSavings := t.savingsBalance
+	if((currentSavings - dollarAmount) < 0.0 || currentSavings == 0.0) {
+            return "Can not buy that much - no cash"
+	} else {
+	    t.coinBalancesMap[coin] = current + coinAmount
+	    t.savingsBalance = currentSavings - dollarAmount
+            ret := fmt.Sprintf("Buy of %.6f %s successful", coinAmount, coin)
+	    return ret
+        }
+  } else if (op == 2) {
+	current := t.coinBalancesMap[coin]
+	if((current - coinAmount) < 0.0 || current == 0.0) {
+            return "Can not sell that much"
+	} else {
+	     t.coinBalancesMap[coin] = current - coinAmount
+             t.savingsBalance = t.savingsBalance + dollarAmount
+	     ret := fmt.Sprintf("Sell of %.6f %s successful", coinAmount, coin)
+	     return ret
+        }
+    } else {
+         return "Invalid Operation"
+    }
+}
+
+
+
